d67ee2baa8406cbca7a613f75b86aa1f47ce5b92
Pavan Kumar Velagana <126136343+pa1velagana@users.noreply.github.com>
2024-07-19 12:48:36 +0530

    Delete HRMS directory

--- HRMS/src/main/java/com/SpyDTech/HRMS/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.SpyDTech.HRMS.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-
-    UserDetailsService userDetailsService();
-}
