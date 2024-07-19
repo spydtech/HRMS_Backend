f52291f7127e8121cbcb52ae5db98ab10f25fd03
Ganesh Nitta <84185648+NITTAGANESH@users.noreply.github.com>
2024-07-19 13:54:00 +0530

    Add files via upload

---
+++ HRMS/src/main/java/com/SpyDTech/HRMS/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.SpyDTech.HRMS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+}
